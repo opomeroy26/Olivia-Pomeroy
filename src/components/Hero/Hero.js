3e71f5c17743ad4558ffb79937827e1749755f0f
Olivia Pomeroy <opomeroy26@gmail.com>
2022-07-18 13:17:04 -0700

    Fixed syntax error

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
         My Portfolio!
       </SectionTitle>
       <SectionText>
-      I enjoy learning new skills and building valuable, user-friendly applications. I am excited to keep growing, expanding my knowledge and taking creative-risks, to contribute my skills and experiences into building meaningful applications.
+      I enjoy learning new skills and building valuable, user-friendly applications. I am excited to keep growing, expanding my knowledge, and taking creative-risks to contribute my skills and experiences into building meaningful applications.
   
       </SectionText>
       <Button onClick={()=> window.location = "mailto:opomeroy26@gmail.com"}>Email Me</Button>
